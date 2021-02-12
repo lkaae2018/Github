109b6953b8a11542626fd93d0b4ea28e8f37e894
********* <**********>
2021-02-12 10:26:04 +0100

    OPDATE

---
+++ githubcommit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#cd linuxopg
+sudo git add *
+sudo git config --global user.email "**********"
+sudo git config --global user.name "*********"
+echo Navn til commiten?
+read commit
+sudo git commit -m $commit
+sudo git push
